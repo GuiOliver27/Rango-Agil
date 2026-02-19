490427d87b6849dc1cccc8e67dc60c3a2b7cad22
Guilherme Monção <guilherme_oliveira27@outlook.com>
2026-02-18 21:20:58 -0400

    Feat: Implementação do EF Core

--- RangoAgilAPI/DbContexts/RangoDbContext.cs
+++ RangoAgilAPI/DbContexts/RangoDbContext.cs
@@ -1,5 +1,14 @@
-﻿namespace RangoAgilAPI.DbContexts;
-public class RangoDbContext {
-    public int MyProperty { get; set; }
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RangoAgilAPI.Entities;
+
+namespace RangoAgilAPI.DbContexts;
+public class RangoDbContext(DbContextOptions<RangoDbContext> options) : DbContext(options) {
+    public DbSet<Rango> Rangos { get; set; } = null;
+    public DbSet<Ingrediente> Ingredientes { get; set; } = null;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+    }
 }
 
