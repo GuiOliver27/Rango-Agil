690224cbd1d350a1da310e4587b2aae2e9b9ebc6
Guilherme Monção <guilherme_oliveira27@outlook.com>
2026-02-25 20:45:16 -0400

    Feat: Criação de Endpoint

--- RangoAgilAPI/Program.cs
+++ RangoAgilAPI/Program.cs
@@ -1,14 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using RangoAgilAPI.DbContexts;
 
-var builder = WebApplication.CreateBuilder(args);
+var webApp = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<RangoDbContext>(
-    o => o.UseSqlite(builder.Configuration["ConnectionStrings:RangoDbConStr"]));
+webApp.Services.AddDbContext<RangoDbContext>(
+    o => o.UseSqlite(webApp.Configuration["ConnectionStrings:RangoDbConStr"]));
 
 // Add services to the container.
 
-var app = builder.Build();
+var app = webApp.Build();
 
 // Configure the HTTP request pipeline.
 
@@ -19,16 +19,11 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () => {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/rangos", () => {
+    
+    return "Está funcionando muito bem!!";
 });
 
 app.Run();
